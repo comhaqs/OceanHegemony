c3794f063d76537c112ee84d9b86f8cf1f4231f8
comahqs <comahqs@163.com>
2021-01-11 17:23:33 +0800

    1、创建项目

---
+++ Assets/Resources/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    Transform main_camera;
+    public Vector3 offset = Vector3.zero;
+
+    void Start()
+    {
+        //设置相对偏移
+        main_camera = Camera.main.gameObject.transform;
+    }
+
+    void Update()
+    {
+        var pos = transform.position + offset;
+        main_camera.position = pos;
+    }
+}
